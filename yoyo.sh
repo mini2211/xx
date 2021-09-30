429a541aaa7d2a47fb427bcdc83afb344a6fe8f5
mini2211 <91670014+mini2211@users.noreply.github.com>
2021-09-30 17:49:23 +0630

    Create yoyo.sh

---
+++ yoyo.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u D8hichWVPBxM7NGzxs6bMj76ZjCLaUnHat.age -p c=DGB -t 4
